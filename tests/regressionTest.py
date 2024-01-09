a42b31c41b4bc8c89fc669d7459549f111a68464
abocchieri <58187203+abocchieri@users.noreply.github.com>
2024-01-09 19:57:04 +0100

    small fix in regressionTest.py

--- tests/regressionTest.py
+++ tests/regressionTest.py
@@ -5,15 +5,17 @@ import unittest
 
 import h5py
 import numpy as np
+from pathlib import Path
 
 from paos.core.pipeline import pipeline
 from paos.log import disableLogging
 
 disableLogging()
 
-paospath = "~/git/PAOS"
-regression_dir = os.path.join(os.path.expanduser(paospath), 'tests', 'regression data')
-conf_file = os.path.join(regression_dir, 'Hubble_simple.ini')
+this_directory = Path(__file__).parent
+
+regression_dir = os.path.join(this_directory, "regression data")
+conf_file = os.path.join(regression_dir, "Hubble_simple.ini")
 
 
 def new_out():
@@ -28,58 +30,61 @@ def new_out():
     """
 
     time_test = time.strftime("%Y%m%d-%H%M%S")
-    out_file = os.path.join(regression_dir, 'regression-{}.h5'.format(time_test))
+    out_file = os.path.join(regression_dir, "regression-{}.h5".format(time_test))
 
-    pipeline(passvalue={'conf': conf_file,
-                        'output': out_file,
-                        'wavelengths': '1.0',
-                        'store_keys': None})
+    pipeline(
+        passvalue={
+            "conf": conf_file,
+            "output": out_file,
+            "wavelengths": "1.0",
+        }
+    )
 
     return out_file
 
 
 class RegressionTest(unittest.TestCase):
     # looks for older PAOS file
-    old_file = glob.glob(os.path.join(regression_dir, '*.h5'))
+    old_file = glob.glob(os.path.join(regression_dir, "*.h5"))
 
     if not old_file:
         # if no older file is in the regression data dir, a new one is produced but the test is failed
         new_out()
         raise FileNotFoundError(
-            'previous run not found. New output file produced. '
-            'Check your results before running the test again.')
+            "previous run not found. New output file produced. "
+            "Check your results before running the test again."
+        )
     else:
         old_file = old_file[0]
 
     # produce a new ExoSim output
-    print('Producing a new PAOS output')
+    print("Producing a new PAOS output")
     new_file = new_out()
 
     def test_content(self):
-        old_f = h5py.File(self.old_file, 'r')
-        new_f = h5py.File(self.new_file, 'r')
+        old_f = h5py.File(self.old_file, "r")
+        new_f = h5py.File(self.new_file, "r")
 
-        print('iteratively checking the output dictionaries')
+        print("iteratively checking the output dictionaries")
         # full run
         self.check_dict_key(new_f, old_f)
         old_f.close()
         new_f.close()
         os.remove(self.old_file)
-        print('Test passed: old version product removed')
+        print("Test passed: old version product removed")
 
     def check_dict_key(self, input1, input2):
         for key in input1.keys():
             print(key)
-            if key == 'info':
+            if key == "info":
                 continue
             if key in input2.keys():
                 if isinstance(input1[key], h5py.Dataset):
                     try:
                         self.assertEqual(input1[key][()], input2[key][()])
                     except ValueError:
-                        np.testing.assert_array_equal(input1[key][()],
-                                                      input2[key][()])
+                        np.testing.assert_array_equal(input1[key][()], input2[key][()])
                 else:
                     self.check_dict_key(input1[key], input2[key])
             else:
-                print(key, 'not in old product')
+                print(key, "not in old product")
