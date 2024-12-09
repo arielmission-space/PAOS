deafde0c386567bac6d3c6f6d766c1ed9354cffe
Andrea Bocchieri <58187203+abocchieri@users.noreply.github.com>
2024-12-09 20:32:19 +0100

    manually increased version

--- __version__.py
+++ __version__.py
@@ -3,4 +3,4 @@ This module contains the version information for the PAOS package.
 
 The version is stored in the `__version__` variable.
 """
-__version__ = "1.2.5-post0"
+__version__ = "1.2.5-dev1"
